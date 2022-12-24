b9d6ac6d70b6afb0530b64001494e4ee65b7bab7
alex-bradbury <>
2022-12-24 11:46:43 +0000

    day 7 setup

---
+++ days/day7/day7.go
@@ -0,0 +1,28 @@
+package day7
+
+import (
+	"fmt"
+)
+
+type Day struct {
+	input string
+}
+
+func (day *Day) Init(filePath string) {
+	//lines := utils.ReadTxtFile(filePath)
+	//day.input = lines[0]
+}
+
+func (day *Day) Task1() string {
+	println("running task 1!")
+	println("---------------")
+
+	return fmt.Sprint(0)
+}
+
+func (day *Day) Task2() string {
+	println("running task 2!")
+	println("---------------")
+
+	return fmt.Sprint(0)
+}
---
+++ days/day7/day7_test.go
@@ -0,0 +1,44 @@
+package day7
+
+import (
+	"testing"
+)
+
+func TestDay7Task1(t *testing.T) {
+	day := new(Day)
+	var tests = []struct {
+		fileName string
+		want     string
+	}{
+		{"../../inputs/day7/day7_test1.txt", "0"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.fileName, func(t *testing.T) {
+			day.Init(tt.fileName)
+			ans := day.Task1()
+			if ans != tt.want {
+				t.Errorf("got %s, want %s", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestDay7Task2(t *testing.T) {
+	day := new(Day)
+	var tests = []struct {
+		fileName string
+		want     string
+	}{
+		{"../../inputs/day7/day7_test1.txt", "0"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.fileName, func(t *testing.T) {
+			day.Init(tt.fileName)
+			day.Task1()
+			ans := day.Task2()
+			if ans != tt.want {
+				t.Errorf("got %s, want %s", ans, tt.want)
+			}
+		})
+	}
+}
